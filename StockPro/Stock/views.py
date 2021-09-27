d1fd0da7c45d56fac8ae7e86ce9ba3580f369447
Bhavy Gupta <bhavygupta1563663@gmail.com>
2021-09-27 22:56:49 +0530

    updated views and requirements.txt

--- StockPro/Stock/views.py
+++ StockPro/Stock/views.py
@@ -1,6 +1,7 @@
+from backend.predict import predict_stock
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
 def stock_predict(request, symbol, period, sim, future):
     data = predict_stock(symbol, period, sim, future)
     return JsonResponse({"data": data})
