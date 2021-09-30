8c951be7467eee7fda763057244d3ed375d344c2
Bhavy Gupta <bhavygupta1563663@gmail.com>
2021-09-30 23:09:49 +0530

    changes in features

--- StockPro/Stock/views.py
+++ StockPro/Stock/views.py
@@ -1,6 +1,11 @@
-from backend.predict import predict_stock
+from Stock.backend.predict import predict_stock
 from django.shortcuts import render
 from django.http import JsonResponse
+
+
+def index(request):
+    return render(request, "index.html")
+
 
 def stock_predict(request, symbol, period, sim, future):
     data = predict_stock(symbol, period, sim, future)
